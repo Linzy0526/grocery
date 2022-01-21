736e4e83fbadae8bf1612bf9260602d85b0e1b2b
Linzy0526 <578039491@qq.com>
2022-01-21 08:55:20 +0000

    Deploying to gh-pages from  @ 8071546e4176be61195db5368c52bf209eafab92 🚀

---
+++ precache-manifest.0065c678b9e71818d032cda8f2ec3f9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea0f818af9a9600d302",
+    "url": "/css/app.87f8e733.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "76205bbd370f9c590f37d517c90764d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adfa73cafa3fa70ebf9a",
+    "url": "/js/about.2df4bf8e.js"
+  },
+  {
+    "revision": "cea0f818af9a9600d302",
+    "url": "/js/app.e17d45ee.js"
+  },
+  {
+    "revision": "7f8f6c11302fd401b6bd",
+    "url": "/js/chunk-vendors.de2f0d89.js"
+  },
+  {
+    "revision": "8005bb5df196b9a26145ec92beeb497a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
